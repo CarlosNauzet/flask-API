20f6729cf13cea990ed788a1790bf8bf2dbbe6af
Carlos Hernández <carlos.hernandez@motork.io>
2023-03-11 23:44:06 +0100

    feat: cargamos los movimientos al cargar la página

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -7,10 +7,32 @@ function cargarMovimientos() {
   peticion.open("GET", "http://127.0.0.1:5000/api/v1/movimientos", false);
   peticion.send();
   console.log(peticion.responseText);
+  const respuesta = JSON.parse(peticion.responseText);
+  const movimientos = respuesta.results;
+
+  let html = "";
+  for (let i = 0; i < movimientos.length; i = i + 1) {
+    const mov = movimientos[i];
+    console.log("Movimiento", mov);
+    html =
+      html +
+      `
+      <tr>
+          <td>${mov.fecha}</td>
+          <td>${mov.concepto}</td>
+          <td>${mov.tipo}</td>
+          <td>${mov.cantidad}</td>
+        </tr>
+      `;
+  }
+
+  const tabla = document.querySelector("#cuerpo-tabla");
+  tabla.innerHTML += html;
 }
 
 window.onload = function () {
   console.log("Función anónima al finalizar la carga de la ventana");
   const boton = document.querySelector("#boton-recarga");
   boton.addEventListener("click", cargarMovimientos);
+  cargarMovimientos();
 };
